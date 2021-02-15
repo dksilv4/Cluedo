6d57813450fb0092db2f36395a48312fc35054d6
Diogo da Silva <diogo.dk.silva@outlook.com>
2021-02-15 13:41:24 +0000

    New Classes
    Added basic new classes that were shown in the spring class diagram.

---
+++ src/Cluedo/Player.java
@@ -0,0 +1,10 @@
+package Cluedo;
+
+public class Player {
+    String name;
+    int[][] location;
+    public Player(String name){
+        this.name= name;
+        this.location = new int[0][0];
+    }
+}
