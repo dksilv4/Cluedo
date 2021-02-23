b969be407998c77e44d2e6ac59f7a9791f731f58
Diogo da Silva <diogo.dk.silva@outlook.com>
2021-02-23 07:51:11 +0000

    changes

--- src/Cluedo/Data.java
+++ src/Cluedo/Data.java
@@ -2,6 +2,7 @@ package Cluedo;
 
 import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Dictionary;
 import java.util.Scanner; // Import the Scanner class to read text files
 import org.json.*;
 
@@ -12,18 +13,22 @@ public class Data {
     }
     public void readFile(String file_name){
         if(file_name.substring(file_name.length() - 3, file_name.length()).equals("txt")){
-            this.readTxt(file_name);
+            this.board = this.readTxt(file_name);
         }
         else if(file_name.substring(file_name.length() - 4, file_name.length()).equals("json")){
             this.readJSON(file_name);
         }
     }
     private void readJSON(String file_name){
-
+        String data = readTxt(file_name); //assign your JSON String here
+        if (data != null) {
+            JSONObject jsonData = new JSONObject(data);
+        }
     }
-    private void readTxt(String file_name){
+
+    private String readTxt(String file_name){
         try {
-            File file = new File("src/Cluedo/"+file_name);
+            File file = new File("src/Cluedo/Data/"+file_name);
             System.out.println(file.getAbsolutePath());
             Scanner myReader = new Scanner(file);
             StringBuilder data = new StringBuilder();
@@ -31,11 +36,12 @@ public class Data {
                 data.append(myReader.nextLine());
                 data.append("\n");
             }
-            this.board = data.toString();
             myReader.close();
+            return data.toString();
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
+            return null;
         }
     }
 
