f096804ecf1ce93539d75b258708663c71d5e2a8
Diogo da Silva <diogo.dk.silva@outlook.com>
2021-02-25 11:12:51 +0000

    update board class
    board class is now able to assign doors to the correct room depending in its placement on the txt file map

--- src/Cluedo/Room.java
+++ src/Cluedo/Room.java
@@ -1,15 +1,37 @@
 package Cluedo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Room {
     String name;
+    List<String> doors = new ArrayList<String>();
+    List<List<Tile>> tiles  = new ArrayList<List<Tile>>();
+    int size_x;
+    int size_y;
     public Room(String name){
         this.name = name;
         this.checkName();
     }
     public String toString(){
-        return "Room:"+this.name;
+//        return "Room:"+this.name;
+        return "\u001B[32m"+this.name+"\u001B[0m"+this.doors;
     }
-
+    public void setSize(int size_x, int size_y){
+        this.size_x = size_x;
+        this.size_y = size_y;
+        for(int x=0;x<size_x;x++){
+            List<Tile> row = new ArrayList<>();
+            for(int y=0;y<size_y;y++){
+                row.add(new Tile("space", x, y));
+            }
+            this.tiles.add(row);
+        }
+        System.out.println(this.tiles);
+    }
+    public void changeTile(int row, int column, Tile tile){
+        this.tiles.get(row).set(column, tile);
+    }
     private void checkName(){
         if(this.name.length()>12){
             throw new InvalidRoomNameException(this.name, "length of name has to be 12 or lower.");
--- src/Cluedo/Tile.java
+++ src/Cluedo/Tile.java
@@ -1,10 +1,30 @@
 package Cluedo;
 
 public class Tile {
-    public Tile(){
-
+    String type;
+    Room belongsTo;
+    int row;
+    int column;
+    public Tile(String type, int row, int column){
+        this.type = type;
+        this.row = row;
+        this.column = column;
     }
     public String toString(){
-        return "x";
+        switch (type){
+            case "door":
+                return "\u001B[31md\u001B[0m";
+            case "room":
+                return belongsTo.toString();
+            case "wall":
+                return "\u001B[34mw\u001B[0m";
+            default:
+                return "x";
+
+        }
+    }
+    public void setBelongsTo(Room room){
+        this.belongsTo = room;
+
     }
 }
