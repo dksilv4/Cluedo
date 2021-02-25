69c2f6f1c0ea853f3c54bb6f8b5d63c76058ff84
hayden <mikaela2080@gmail.com>
2021-02-25 21:56:41 +0000

    tile

--- src/Cluedo/Tile.java
+++ src/Cluedo/Tile.java
@@ -1,15 +1,43 @@
 package Cluedo;
 
 public class Tile {
+
     String type;
     Room belongsTo;
     int row;
     int column;
+    boolean isOccupied;
+
     public Tile(String type, int row, int column){
         this.type = type;
         this.row = row;
         this.column = column;
     }
+
+    public boolean isOccupied() {
+        return isOccupied;
+    }
+
+    public void setOccupied(boolean occupied) {
+        isOccupied = occupied;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setBelongsTo(Room room){
+        this.belongsTo = room;
+
+    }
+
+    public boolean available(PlayerPiece piece){
+        if(this.isOccupied() == false && piece.movingState() > 0 && this.getType() != "wall"){
+            return true;
+        }
+        return false;
+    }
+
     public String toString(){
         switch (type){
             case "door":
@@ -22,9 +50,5 @@ public class Tile {
                 return "x";
 
         }
-    }
-    public void setBelongsTo(Room room){
-        this.belongsTo = room;
-
     }
 }
