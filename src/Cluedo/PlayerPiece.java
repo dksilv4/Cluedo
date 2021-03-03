13ba87cc1ff16fc0b6705860a65290159793c362
pioneerkiller542 <pioneerkiller542@users.noreply.github.com>
2021-03-03 14:07:41 +0000

    Finished setting up rooms and player
    Rooms are all where they're supposed to be also Player have been set up dynamically

--- src/Cluedo/PlayerPiece.java
+++ src/Cluedo/PlayerPiece.java
@@ -1,6 +1,12 @@
 package Cluedo;
+import java.io.File;
+import java.util.*;
 
 public class PlayerPiece {
     public PlayerPiece(){
+        List list = Arrays.asList(objectMapper.readValue(new File("data.json"), PlayerPiece[].class));
+
+
     }
-}
+    }
+
