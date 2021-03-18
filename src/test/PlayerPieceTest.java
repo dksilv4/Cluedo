41695c55b3beadc68d767dfaf3aaaaead17ba4be
Diogo da Silva <diogo.dk.silva@outlook.com>
2021-03-18 18:49:13 +0000

    Update PlayerPieceTest.java

--- src/test/PlayerPieceTest.java
+++ src/test/PlayerPieceTest.java
@@ -1,26 +1,5 @@
 
 
-import code.PlayerPiece;
-import org.junit.Before;
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
-
 public class PlayerPieceTest {
-    private PlayerPiece playerPiece = new PlayerPiece("ii");
 
-    @Before
-    public void setUp(){
-
-    }
-
-    @Test
-    public void testGetName(){
-        assertEquals("ii", this.playerPiece.getName());
-    }
-
-    @Test
-    public void testToString(){
-        assertEquals("PlayerPiece: ii", this.playerPiece.toString());
-    }
 }
